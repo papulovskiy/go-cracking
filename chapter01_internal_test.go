b9741ddafc964ea677166f996cf777c514f6f9b4
Alexey Papulovskiy <a@c17.nl>
2015-12-25 12:25:15 +0100

    Formatting

--- chapter01_internal_test.go
+++ chapter01_internal_test.go
@@ -3,19 +3,19 @@ package main
 import "testing"
 
 func TestStringUniqueCharacters(t *testing.T) {
-    if StringUniqueCharacters("abcdef") != true {
-        t.Error("String contains unique characters")
-    }
-    if StringUniqueCharacters("abcdefa") != false {
-        t.Error("String does not contain unique characters")
-    }
-    if StringUniqueCharacters("") != false {
-        t.Error("String is empty")
-    }
-    if StringUniqueCharacters("⌘") != true {
-        t.Error("String contains unique characters")
-    }
-    if StringUniqueCharacters("⌘⌘") != false {
-        t.Error("String does not contain unique characters")
-    }
+	if StringUniqueCharacters("abcdef") != true {
+		t.Error("String contains unique characters")
+	}
+	if StringUniqueCharacters("abcdefa") != false {
+		t.Error("String does not contain unique characters")
+	}
+	if StringUniqueCharacters("") != false {
+		t.Error("String is empty")
+	}
+	if StringUniqueCharacters("⌘") != true {
+		t.Error("String contains unique characters")
+	}
+	if StringUniqueCharacters("⌘⌘") != false {
+		t.Error("String does not contain unique characters")
+	}
 }
